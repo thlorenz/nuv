1ed01c91bc49875f3b5f4f5283879a87744078a8
Thorsten Lorenz <thlorenz@gmx.de>
2014-08-02 18:53:53 -0400

    working build for v0.10.28

---
+++ Makefile
@@ -0,0 +1,23 @@
+include uv.mk
+
+INCLUDES=
+LDFLAGS=
+CFLAGS=-g -O2 -Wall
+LIBS=
+
+INCLUDES +=$(uv_INCLUDES)
+CFLAGS+=$(uv_CFLAGS)
+LDFLAGS += $(uv_LDFLAGS)
+LIBS += $(uv_LIBS)
+
+examples/%: $(uv_LIBA)
+	$(CC) $(CFLAGS) $(LDFLAGS) $(INCLUDES) $(LIBS) $(@:%=%.c) $^ -o $@
+
+clean:
+		find . -name "*.gc*" -exec rm {} \;
+		rm -rf `find . -name "*.dSYM" -print`
+		rm -f `find libuv -name *.o` 
+		rm -f examples/idle-basic
+		$(MAKE) -C $(uv_TOPDIR) $@
+
+.PHONY: clean 
---
+++ uv.mk
@@ -0,0 +1,34 @@
+TOPDIR?= $(shell pwd)
+
+uv_TOPDIR=$(TOPDIR)/libuv
+uv_LIBA=$(uv_TOPDIR)/libuv.a
+
+uv_INCLUDES = -I$(uv_TOPDIR)/include -I$(uv_TOPDIR)/src
+
+uv_CFLAGS = 
+uv_LDFLAGS=
+uv_LIBS=
+
+uname_S=$(shell uname -s)
+ifeq (Darwin, $(uname_S))
+uv_CFLAGS+=-D_DARWIN_USE_64_BIT_INODE=1
+uv_CFLAGS+=-framework CoreServices
+uv_LDFLAGS+=-framework Foundation   \
+						-framework CoreFoundation \
+						-framework ApplicationServices
+endif
+
+ifeq (Linux, $(uname_S))
+	uv_LIBS += -lrt -ldl -lm -pthread
+endif
+
+$(uv_TOPDIR):
+	git clone https://github.com/joyent/libuv.git
+
+$(uv_LIBA): $(uv_TOPDIR)
+	cd $(uv_TOPDIR) &&                                                                                              \
+	test -d ./build/gyp || (mkdir -p ./build && git clone https://git.chromium.org/external/gyp.git ./build/gyp) && \
+	./gyp_uv.py -f make &&                                                                                          \
+	cd $(uv_TOPDIR) && $(MAKE) -j 8
+
+uv_la_HEADERS=include/uv.h include/uv-errno.h include/uv-threadpool.h include/uv-version.h
