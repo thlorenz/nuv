97567f990cd98059237606bf57bd6da8448c3411
Thorsten Lorenz <thlorenz@gmx.de>
2014-08-03 23:17:41 -0400

    link libuv task

--- uv.mk
+++ uv.mk
@@ -12,6 +12,12 @@ $(uv_LIBA): $(uv_TOPDIR)
 	# uv v0.10.x doesn't include patch version in a header file, so we need to fix that
 	test -f $(uv_VERSION_HEADER) || cat libuv/src/version.c | grep '#define UV_VERSION_PATCH' >> $(uv_VERSION_HEADER)
 
+# used to switch between versions quickly simply linking the libuv dir
+# this is needed to make tools like vim work, i.e. .clang_complete includes libuv
+ln_%:
+	rm -rf $(TOPDIR)/libuv
+	ln -s  $(TOPDIR)/libuv$(@:ln_%=%) $(TOPDIR)/libuv
+
 uv_v10:
 	rm -f $(uv_LIBA)
 	cd $(uv_TOPDIR) && git reset HEAD --hard --quiet  && git checkout v0.10.28 --force
